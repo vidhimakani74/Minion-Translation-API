0c332a77fcc4f1267e897ad0fa2fe797aaae78c5
Vidhi Makani <100896986+vidhimakani74@users.noreply.github.com>
2022-11-06 18:56:55 +0530

    input output added

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-console.log("hello! script is working in new file")
+// console.log("hello! script is working in new file")
  
-var username =prompt("give me ypur user name");
+// var username =prompt("give me ypur user name");
 
-alert("this script works!"+username);
+var btntranslate=document.querySelector("#btn-translate")
+var txtInput=document.querySelector("#txt-input")
+console.log(txtInput)
+btntranslate.addEventListener("click", function clickEventHandler(){
+    console.log("clicked");
+    console.log("input",txtInput.value);
+}) 
