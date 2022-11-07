f302ed74a3445f640cfc96ea0e71d5e700984d9d
Vidhi Makani <100896986+vidhimakani74@users.noreply.github.com>
2022-11-07 22:08:00 +0530

    output functions added

--- app.js
+++ app.js
@@ -2,31 +2,40 @@
  
 // var username =prompt("give me ypur user name");
 
-var btntranslate=document.querySelector("#btn-translate")
+var btntranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input")
 var outputDiv=document.querySelector("#output")
 
+// var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I am vidhi makani"
 
-var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I am vidhi makani"
+var serverUrl="https://api.funtranslations.com/translate/minion.json"
 
-function getTranslationURL(text){
-    return serverURL + "?"+ "text"+ text
+function getTranslationURL(input){
+    return serverUrl + "?"+ "text="+ input
 }
-
-
-
 
 // console.log(outputDiv);
 // outputDiv.innerText="vidhi makani"
 // console.log(txtInput)
-
-btntranslate.addEventListener("click", function clickEventHandler(){
-    // console.log("clicked");
+function errorEventHandler(error){
+    console.log("error occured",error)
+    alert("somthing wrong with server! try again after some time")
+}
+// btntranslate.addEventListener("click", function clickEventHandler{
+    // console.log("clicked");   z 
     // console.log("input",txtInput.value);
     // outputDiv.innerText="asasasasaas"+ txtInput.value;
-    
-    var inputText=txtInput.ariaValueMax; //taking value
+
+function clickHandler(){
+    var inputText=txtInput.value; //taking value
+
     fetch(getTranslationURL(inputText)) //calling server for processing
-    .then(response=>response.json())
-    .then(json=>console.log(json))
-}) 
+        .then(response=>response.json())
+        //.then(json=>//console.log(json.contents.translated))
+        .then(json=>{
+            var translatedText=json.contents.translated;
+            outputDiv.innerText=translatedText;
+         })
+        .catch(errorEventHandler)
+}
+btnTranslate.addEventListener("click", clickHandler);
